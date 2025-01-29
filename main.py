1ce50a724e9175ac1c3ebf1f80db4a064d1c39be
thoria-subahi <thoria.s@outlook.com>
2025-01-29 21:44:54 +0000

    Changed Room Layout

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from room import Room #im importing Room (name of class)FROM room (name of file)
 
-kitchen = Room("Kitchen") #setting room name to kitchen
-ballroom = Room("Ballroom")
-dining_hall = Room("Dining hall")
-# print(kitchen.name) 
+dungeon = Room("Dungeon")
+dungeon.description = "A scary room filled with rats and monsters"
 
-kitchen.set_description("A dark and dirty room buzzing with flies")
+royal_chamber = Room("Royal Chamber")  
+royal_chamber.description = "A bright room with a large throne and soft lights"
+
+courtyard = Room("Courtyard")
+courtyard.description = "A large outdoor space with a beautiful garden"
+
+dungeon.link_room(royal_chamber, "north")
+royal_chamber.link_room(dungeon, "south")
+
+royal_chamber.link_room(courtyard, "east")
+courtyard.link_room(royal_chamber, "west")
+
+dungeon.get_details()
+royal_chamber.get_details()
+courtyard.get_details()
+
+
+
