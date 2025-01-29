05f1caaf73a08a6d3c9a74f51b6e82fc530478a9
thoria-subahi <thoria.s@outlook.com>
2025-01-29 23:49:57 +0000

    added player inventory

--- room.py
+++ room.py
@@ -3,6 +3,8 @@ class Room:
         self.name = room_name
         self.description = None
         self.linked_rooms = {}
+        self.items = []
+        self.character = None
 
     #creating getter and setter
 
@@ -25,14 +27,41 @@ class Room:
     def describe(self):
         print(self.description)
 
+
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link #setting a key value pair (direction is a key), room_to_link is value..
 
+    def set_character(self, new_character):
+        self.character = new_character
 
-    def get_details(self):
-        print(self.name)
+    def get_character(self):
+        return self.character
+
+    def add_item(self, item):
+        self.items.append(item)
+        print(f"Added {item.name} to the {self.name}")
+
+    def get_details(self, player_inventory):
+        print(f"\nRight now you are in the {self.name}")
         print("-------------------------")
         print(self.description)
+
         for direction, room in self.linked_rooms.items():
             print(f"The {room.name} is {direction}")
-    
+        if self.items:
+            print("\nItems in the room:")
+            for item in self.items:
+                print(f"{item.name}: {item.description}")
+        if self.character:
+            print(f"\nThere is someone here: {self.character.name}")
+            
+
+        
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+        else:
+            print("You are forbidden from entering through this path!")
+            return self
+
+   
