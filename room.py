eb9b3899003d3a8e1c249d096e9afb4cbcf8a960
thoria-subahi <thoria.s@outlook.com>
2025-01-29 21:44:24 +0000

    Added @property Decorator

--- room.py
+++ room.py
@@ -2,13 +2,37 @@ class Room:
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+        self.linked_rooms = {}
 
     #creating getter and setter
-    def get_description(self):
-        return self.description 
 
-    def set_description(self, room_description):
-        self.description = room_description
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, new_description):
+        self._description = new_description
+
+    def describe(self):
+        print(self.description)
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link #setting a key value pair (direction is a key), room_to_link is value..
 
 
+    def get_details(self):
+        print(self.name)
+        print("-------------------------")
+        print(self.description)
+        for direction, room in self.linked_rooms.items():
+            print(f"The {room.name} is {direction}")
     
